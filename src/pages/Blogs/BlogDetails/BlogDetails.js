e081058a3730c259a1d958dddbe554a97944e7fb
Dipra Das <dipradas5940@gmail.com>
2023-03-02 00:32:33 +0600

    Comment Posting

--- src/pages/Blogs/BlogDetails/BlogDetails.js
+++ src/pages/Blogs/BlogDetails/BlogDetails.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { CiChat1 } from "react-icons/ci";
 import './BlogDetails.css';
+import { AuthContext } from '../../../contexts/AuthProvider';
+import Swal from 'sweetalert2';
 
 const BlogDetails = () => {
     const formBlogs = useLoaderData();
     const blog = (formBlogs[0]);
+
+    const { user } = useContext(AuthContext);
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const comment = useRef();
+
+
+    const handleCommentPost = e => {
+        const commentData = {
+            userName: user.displayName,
+            userEmail: user.email,
+            blogId: blog._id,
+            comment: comment.current.value,
+            postedMonth: months[new Date().getMonth()],
+            postedYear: new Date().getFullYear()
+        }
+        fetch('http://localhost:5000/comments', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(commentData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: 'Comment Posted',
+                        showConfirmButton: false,
+                        timer: 2000
+                    })
+                    e.target.reset();
+                }
+            })
+        e.preventDefault();
+
+    }
     return (
         <div>
             <div className="backGroundBlogDetails">
@@ -14,6 +55,21 @@ const BlogDetails = () => {
                     <div className='backGroundBlogDetailsTitle text-center'>{blog.blogTitle}</div>
                     <div className="backGroundBlogDetailsTimeComment text-center">{blog.today} / by KOSATeam / <span> <CiChat1></CiChat1> 0</span> </div>
                 </div>
+            </div>
+            <div className='container mx-auto p-5'>
+                <div className='backGroundBlogDescription'>{blog.description}</div>
+                <div className='backGroundBlogColon'>“</div>
+                <div className='backGroundBlogQuote'>{blog.quote}</div>
+                <hr />
+            </div>
+            <div className='container mx-auto blogDetailsComment px-5'>
+                <h4>Leave a Reply</h4>
+                <h5>Your email address will not be published. Required fields are marked *</h5>
+                <h5>Comment *</h5>
+                <form onSubmit={handleCommentPost}>
+                    <textarea name="comment" ref={comment} id="comment" cols="120" rows="6"></textarea> <br />
+                    <button type="submit" className='blogButton mt-4'>Post Comment</button>
+                </form>
             </div>
         </div>
     );
